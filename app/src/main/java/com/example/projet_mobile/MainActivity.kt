53450907b86c0f5029e2cef2ca2f28e2e6e18c92
BenocxX <mathis.cote@outlook.com>
2021-11-25 15:58:57 -0500

    Added fragment with drawer

--- app/src/main/java/com/example/projet_mobile/MainActivity.kt
+++ app/src/main/java/com/example/projet_mobile/MainActivity.kt
@@ -2,15 +2,16 @@ package com.example.projet_mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var currentFragment: Fragment
     private lateinit var drawerLayout : DrawerLayout
     private lateinit var navigationView : NavigationView
     private lateinit var toggle : ActionBarDrawerToggle
@@ -19,7 +20,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        changeFragment(MainPageFragment())
         initializeDrawerMenu()
+        initalizeDrawerNavigation()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -34,22 +37,27 @@ class MainActivity : AppCompatActivity() {
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
-
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
 
+    private fun initalizeDrawerNavigation() {
         navigationView = findViewById(R.id.navView)
         navigationView.setNavigationItemSelectedListener {
             when (it.itemId) {
-                R.id.iMainPage -> changeFragment("Main Page")
-                R.id.iProfile -> changeFragment("Profile")
-                R.id.iCart -> changeFragment("Cart")
+                R.id.iMainPage -> changeFragment(MainPageFragment())
+                R.id.iProfile -> changeFragment(ProfileFragment())
+                R.id.iCart -> changeFragment(CartFragment())
             }
             drawerLayout.closeDrawer(GravityCompat.START)
             true
         }
     }
 
-    private fun changeFragment(itemName: String) {
-        Log.d("TEST1", "Clicked on $itemName")
+    private fun changeFragment(fragment: Fragment) {
+        currentFragment = fragment
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, currentFragment)
+            commit()
+        }
     }
 }
---
+++ app/src/main/java/com/example/projet_mobile/ProfileFragment.kt
@@ -0,0 +1,7 @@
+package com.example.projet_mobile
+
+import androidx.fragment.app.Fragment
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+}
