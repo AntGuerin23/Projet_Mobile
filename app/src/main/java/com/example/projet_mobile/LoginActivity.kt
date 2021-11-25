cf82f3b828cc9feee616f999c008b0e8353a2dd8
Antoine Guérin <90921460+AntGuerin23@users.noreply.github.com>
2021-11-25 15:01:35 -0500

    Added buttons to login activty

--- app/src/main/java/com/example/projet_mobile/LoginActivity.kt
+++ app/src/main/java/com/example/projet_mobile/LoginActivity.kt
@@ -1,13 +1,11 @@
 package com.example.projet_mobile
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
