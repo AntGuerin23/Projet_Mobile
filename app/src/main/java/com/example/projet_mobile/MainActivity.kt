10e530b7c6e7cb292cce91098db923b17ec2ca09
BenocxX <mathis.cote@outlook.com>
2021-11-25 15:37:27 -0500

    Added a drawer to MainActivity

--- app/src/main/java/com/example/projet_mobile/MainActivity.kt
+++ app/src/main/java/com/example/projet_mobile/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.projet_mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout : DrawerLayout
+    private lateinit var navigationView : NavigationView
+    private lateinit var toggle : ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initializeDrawerMenu()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun initializeDrawerMenu() {
+        drawerLayout = findViewById(R.id.drawerLayout)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navigationView = findViewById(R.id.navView)
+        navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.iMainPage -> changeFragment("Main Page")
+                R.id.iProfile -> changeFragment("Profile")
+                R.id.iCart -> changeFragment("Cart")
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+    }
+
+    private fun changeFragment(itemName: String) {
+        Log.d("TEST1", "Clicked on $itemName")
     }
 }
