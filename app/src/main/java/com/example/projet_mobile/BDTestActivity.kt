b5957c6ca9ac246aee06e67a9f531be4f7f045c7
BenocxX <mathis.cote@outlook.com>
2021-11-25 15:00:44 -0500

    Added new Activity for database test

---
+++ app/src/main/java/com/example/projet_mobile/BDTestActivity.kt
@@ -0,0 +1,13 @@
+package com.example.projet_mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BDTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bdtest)
+
+        // Your code goes here
+    }
+}
