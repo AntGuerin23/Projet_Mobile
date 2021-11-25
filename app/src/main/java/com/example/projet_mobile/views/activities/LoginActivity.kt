392363f2cae8c4358fcded3781606f24596c26c8
BenocxX <mathis.cote@outlook.com>
2021-11-25 17:49:11 -0500

    Added gradient to ActionBar

--- app/src/main/java/com/example/projet_mobile/views/activities/LoginActivity.kt
+++ app/src/main/java/com/example/projet_mobile/views/activities/LoginActivity.kt
@@ -3,13 +3,17 @@ package com.example.projet_mobile.views.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Button
+import androidx.appcompat.app.ActionBar
 import com.example.projet_mobile.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
         findViewById<Button>(R.id.mainActivityButton).setOnClickListener {
             val intent = Intent(this, MainActivity :: class.java)
             startActivity(intent)
