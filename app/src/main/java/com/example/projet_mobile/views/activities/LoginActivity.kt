155858f755a70722c96f862a8d27e0bb3dc2bcc9
BenocxX <mathis.cote@outlook.com>
2021-11-25 21:45:59 -0500

    Removed useless imports

--- app/src/main/java/com/example/projet_mobile/views/activities/LoginActivity.kt
+++ app/src/main/java/com/example/projet_mobile/views/activities/LoginActivity.kt
@@ -1,14 +1,8 @@
 package com.example.projet_mobile.views.activities
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
-import android.widget.Button
-import androidx.appcompat.app.ActionBar
 import com.example.projet_mobile.R
-import android.graphics.drawable.TransitionDrawable
 import androidx.fragment.app.Fragment
 import com.example.projet_mobile.views.fragments.LoginFragment
 
