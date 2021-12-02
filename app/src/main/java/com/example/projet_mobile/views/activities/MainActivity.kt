bc4c5922eecaedac7eed49efc9b4cae124e3abf0
BenocxX <mathis.cote@outlook.com>
2021-12-02 17:14:52 -0500

    Fix navigation bug

--- app/src/main/java/com/example/projet_mobile/views/activities/MainActivity.kt
+++ app/src/main/java/com/example/projet_mobile/views/activities/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         changeFragment(MainPageFragment())
         initializeDrawerMenu()
+        initializeDrawerNavigation()
 
 //        val rows = TableConverter.getRows(Database.query("SELECT * FROM vendeur"))
 //        for (row in rows) {
