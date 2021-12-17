3d8a38742942e82ff0fd31952ac480c33fa04279
BenocxX <mathis.cote@outlook.com>
2021-12-17 01:16:19 -0500

    Added confirmation fragment

---
+++ app/src/main/java/com/example/projet_mobile/views/fragments/ConfirmationFragment.kt
@@ -0,0 +1,35 @@
+package com.example.projet_mobile.views.fragments
+
+import android.os.Bundle
+import android.transition.TransitionInflater
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.example.projet_mobile.R
+import com.example.projet_mobile.views.activities.MainActivity
+
+class ConfirmationFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val inflater = TransitionInflater.from(requireContext())
+        enterTransition = inflater.inflateTransition(R.transition.fade)
+        exitTransition = inflater.inflateTransition(R.transition.fade)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_confirmation, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.bMainPage).setOnClickListener {
+            (activity as MainActivity).changeFragment(ConfirmationFragment())
+        }
+    }
+}
